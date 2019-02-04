fc7c33d5752f0fc106947c8e9984190702eea6c2
ZlofZ <tobias.larsson96@gmail.com>
2019-02-04 10:05:13 +0200

    Add files via upload

--- TullFormAutomationNadendal.user.js
+++ TullFormAutomationNadendal.user.js
@@ -9,20 +9,25 @@
 // ==/UserScript==
 
 
+
 var questionIds = ["Questions_0__Options_0__Answers_0__TextValue","Questions_0__Options_1__Answers_0__TextValue","Questions_0__Options_2__Answers_0__TextValue","Questions_0__Options_3__Answers_0__TextValue","Questions_0__Options_4__Answers_0__TextValue","Questions_0__Options_5__Answers_0__TextValue"];
 var answers = ["Transmar Ab","0144866-7","018-27011","trafikledning@transmar.fi","Clemes Mats","Åland-Finland"];
 
 var button;
 var nextbutton = document.getElementsByClassName("participation-buttons")[0].childNodes[1].childNodes[1];
 
+var getTomorrow = function(){
+    var date = new Date();
+    date.setTime(date.getTime()+ (24 * 60 * 60 * 1000));
+    return date;
+}
 
 var nclick = function(){
     this.surveyParticipationViewModel.doNext();
 }
 
-var putDate = function(){
+var putDate = function(d){
     var date = document.getElementById("Questions_1__Options_0__Answers_0__TextValue");
-    var d = new Date();
     var day = ("0" + d.getDate()).slice(-2);
     var month = ("0" + (d.getMonth() + 1)).slice(-2);
     date.setAttribute("value",day+"."+month+"."+d.getFullYear());
@@ -33,7 +38,7 @@ var page1 = function() {
          var q = document.getElementById(questionIds[i]);
         q.innerHTML += answers[i];
     }
-    putDate();
+    putDate(new Date());
 }
 
 var page2 = function(){
@@ -41,13 +46,13 @@ var page2 = function(){
 }
 
 var page3 = function(){
-    putDate();
+    putDate(getTomorrow());
     p3Fields();
 }
 
 var p3Fields = function(){
     document.getElementById("o_b8f34507-4242-e811-80d6-ee6c6c76c8f5").click();
-    var val = document.getElementById("Questions_2__Options_1__AttachedTextValue").value = "m/s Fjärdvägen 00:30";
+    var val = document.getElementById("Questions_2__Options_1__AttachedTextValue").value = "m/s Fjärdvägen 05:30";
     document.getElementById("o_d4f34507-4242-e811-80d6-ee6c6c76c8f5").click();
     document.getElementById("Questions_3__Options_1__AttachedTextValue").value = "ÅS-5077";
     document.getElementById("Questions_4__Options_0__Answers_0__TextValue").innerHTML = answers[0];
